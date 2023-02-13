1b91795f2de4057ea47f9f88d1e6e2de9d63c988
Wesley Silva <wesley.allansilva@gmail.com>
2023-02-13 01:14:15 -0300

    feat(page): new styles and configuration
    New styles for the page and use of newly added components

--- src/pages/Customers/index.tsx
+++ src/pages/Customers/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { CardsContainer } from '../../components/CardsContainer';
 import { Filter } from '../../components/Filter';
+import * as S from './styles';
 
 export const Customers = () => {
   return (
-    <>
-      <Filter />
-      <CardsContainer />
-    </>
+    <S.Container>
+      <S.FilterContainer>
+        <Filter />
+      </S.FilterContainer>
+      <S.CardsContainer>
+        <CardsContainer />
+      </S.CardsContainer>
+    </S.Container>
   );
 };
